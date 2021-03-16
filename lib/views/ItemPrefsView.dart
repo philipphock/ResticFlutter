04290baee23b08e09a1db9759343203233ba19b5
Philipp Hock <philipphock@gmail.com>
2021-03-16 09:24:33 +0100

    ++

--- lib/views/ItemPrefsView.dart
+++ lib/views/ItemPrefsView.dart
@@ -10,16 +10,69 @@ class ItemPrefsView extends StatelessWidget {
     print(item.heading);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Inspect"),
-        iconTheme: IconThemeData(
-          color: Colors.white70, //change your color here
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text("Inspect"),
+          iconTheme: IconThemeData(
+            color: Colors.white70, //change your color here
+          ),
         ),
-      ),
-    );
-  }
-}
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                "Heading",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 38),
+              )),
+          Padding(
+              padding: EdgeInsets.all(1),
+              child: Text(
+                "Repo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 28),
+              )),
+          Padding(
+              padding: EdgeInsets.all(1),
+              child: Text(
+                "Src",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 28),
+              )),
 
-class ItemPrefsModel extends ChangeNotifier {
-  ItemPrefsModel() : super() {}
+          // List
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) =>
+                          Divider(color: Colors.black, height: 5),
+                      //shrinkWrap: true,
+                      //physics: ClampingScrollPhysics(),
+                      itemCount: 14,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                            color: Colors.black38,
+                            child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 30, horizontal: 5),
+                                child: Row(children: [
+                                  Text("Item"),
+                                  Spacer(),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.file_download)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.folder_open_rounded)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                      ))
+                                ])));
+                      })))
+        ]));
+  }
 }
