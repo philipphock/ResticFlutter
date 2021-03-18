1cb6ee2406500267840dd00ee14a6eb78dc15d08
Philipp Hock <philipphock@gmail.com>
2021-03-18 07:15:27 +0100

    added process

---
+++ lib/process/process.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'dart:io';
+
+enum ProcessInfoEventType { STDERR, STDOUT, EXIT, ERR }
+
+class ProcessInfoEventArgs {
+  final String cmd;
+  final ProcessInfoEventType type;
+  final int exitCode;
+  final String stderr;
+  final String stdout;
+  final String errStartInfo;
+
+  ProcessInfoEventArgs(this.cmd, this.type, this.exitCode, this.stderr,
+      this.stdout, this.errStartInfo);
+
+  @override
+  String toString() {
+    var ret = """
+    command: $cmd
+    type: $type
+    exitCode: $exitCode
+    StdErr: $stderr
+    StdOut: $stdout
+    ErrStartInfo: $errStartInfo
+    """;
+    return ret;
+  }
+}
+
+class ProcessSummary {
+  final String stderr;
+  final String stdout;
+  final int exitCode;
+  final String errStartInfo;
+
+  ProcessSummary(this.stderr, this.stdout, this.exitCode, this.errStartInfo);
+
+  @override
+  String toString() {
+    return """
+    StartInfo: $errStartInfo
+    ExitCode: $exitCode
+    StdOut: $stdout
+    StdErr: $stderr
+    """;
+  }
+}
+
+class ProcessInfo {
+  final Process _p;
+  final Stream<String> _stdout;
+  final Stream<String> _stderr;
+  final StreamController<ProcessInfoEventArgs> _event;
+  final String _cmd;
+
+  ProcessInfo(this._p, this._cmd)
+      : _stdout = _p.stderr.transform(utf8.decoder), //.forEach(print);
+        _stderr = _p.stdout.transform(utf8.decoder), //.forEach(print);
+        _event = StreamController() {
+    (() async {
+      try {
+        _stderr.forEach((element) {
+          _event.add(ProcessInfoEventArgs(
+              _cmd, ProcessInfoEventType.STDERR, -1, element, "", ""));
+        });
+        _stdout.forEach((element) {
+          _event.add(ProcessInfoEventArgs(
+              _cmd, ProcessInfoEventType.STDOUT, -1, "", element, ""));
+        });
+        int exit = await _p.exitCode;
+
+        _event.add(ProcessInfoEventArgs(
+            _cmd, ProcessInfoEventType.EXIT, exit, "", "", ""));
+        _event.close();
+      } catch (e) {
+        ProcessInfoEventArgs(
+            _cmd, ProcessInfoEventType.EXIT, -1, "", "", e.toString());
+        _event.close();
+      }
+    })();
+  }
+
+  Stream<ProcessInfoEventArgs> get stream => _event.stream;
+
+  bool kill() {
+    return _p.kill();
+  }
+
+  Future<int> get exitCode {
+    return _p.exitCode;
+  }
+
+  Future<ProcessSummary> summary() async {
+    final StringBuffer sberr = StringBuffer();
+    final StringBuffer sbout = StringBuffer();
+
+    Completer<ProcessSummary> _exec = Completer();
+    stream.listen((event) {
+      switch (event.type) {
+        case ProcessInfoEventType.STDERR:
+          sberr.write(event.stderr);
+          break;
+        case ProcessInfoEventType.STDOUT:
+          sbout.write(event.stderr);
+          break;
+        case ProcessInfoEventType.EXIT:
+          var c = ProcessSummary(
+              sberr.toString(), sbout.toString(), event.exitCode, "");
+          _exec.complete(c);
+
+          break;
+        case ProcessInfoEventType.ERR:
+          var c = ProcessSummary(
+              sberr.toString(), sbout.toString(), -1, event.errStartInfo);
+          _exec.complete(c);
+          break;
+      }
+    });
+
+    return _exec.future;
+  }
+}
+
+class ProcessExecutor {
+  Future<ProcessInfo> exec(
+      List<String> args, String wd, Map<String, String> env) async {
+    const APP = "restic";
+    //const env = {"RESTIC_PASSWORD": "a"};
+    final process =
+        await Process.start(APP, args, environment: env, workingDirectory: wd);
+
+    return ProcessInfo(process, args[0]);
+  }
+}
+
+class ResticProxy {}
+
+void main() async {
+  var p = ProcessExecutor();
+  var args = ["snapshots"];
+  var wd = r"c:\test";
+  var env = {"RESTIC_PASSWORD": "a", "RESTIC_REPOSITORY": "repo"};
+  var pi = await p.exec(args, wd, env);
+  var sum = await pi.summary();
+  print(sum);
+}
