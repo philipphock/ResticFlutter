05fab90a17c82146c5de24873abf6108e8f595ec
Philipp Hock <philipphock@gmail.com>
2021-03-21 08:27:05 +0100

    added windows folder picker

--- lib/util/Filepicker.dart
+++ lib/util/Filepicker.dart
@@ -1,12 +1,17 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:filepicker_windows/filepicker_windows.dart';
 import 'package:filesystem_picker/filesystem_picker.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:restic_ui/util/dialog.dart';
 
 Future<String> pickFolder(BuildContext context) async {
   final isWin = Platform.isWindows;
+  if (isWin) {
+    return _pickFolderWin(context);
+  }
   var drive = "/";
+  /*
   if (isWin) {
     drive = await showInputDialog(context, "Drive", "drive letter", len: 1);
     if (drive == null) {
@@ -14,10 +19,16 @@ Future<String> pickFolder(BuildContext context) async {
     }
     drive = "$drive:/";
   }
-
+  */
   var p = await FilesystemPicker.open(
       context: context,
       fsType: FilesystemType.folder,
       rootDirectory: Directory.fromUri(Uri.file(drive, windows: isWin)));
   return p;
+}
+
+String _pickFolderWin(BuildContext context) {
+  final file = DirectoryPicker()..title = 'Select a folder';
+  final result = file.getDirectory();
+  return result?.path;
 }
